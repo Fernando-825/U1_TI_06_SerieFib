cbc101a25fe1e97cde0326dba424bafb582c62e9
Fernando-825 <20243ds128@utez.edu.mx>
2025-09-19 21:37:01 -0600

    Commit message

---
+++ U1_TI_06_SerieFib/src/App.java
@@ -0,0 +1,20 @@
+public class App {
+
+        static int fibonacci(int num){
+            if(num <= 1){
+                return num;
+            }
+            return fibonacci(num-1) + fibonacci(num-2);
+        }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        int num=5;
+        fibonacci(5);
+        for(int n = 0; n<num; n++){
+            System.out.println(""+fibonacci(n));
+        }
+
+    }
+}
